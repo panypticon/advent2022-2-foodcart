933ae7dc6b74552680905366338f7379c07cc9e4
Alex <hi@alexmenger.me>
2022-12-12 17:34:04 +0100

    Add inventory data

---
+++ src/app/inventory.js
@@ -0,0 +1,39 @@
+const inventory = {
+    [crypto.randomUUID()]: {
+        name: 'French Fries',
+        price: 5.9,
+        img: 'plate__french-fries'
+    },
+    [crypto.randomUUID()]: {
+        name: 'Soymon and Veg',
+        price: 14.9,
+        img: 'plate__salmon-vegetables'
+    },
+    [crypto.randomUUID()]: {
+        name: 'Meatless Bolognese',
+        price: 10.9,
+        img: 'plate__spaghetti-meat-sauce'
+    },
+    [crypto.randomUUID()]: {
+        name: 'Fake Chicken Caesar',
+        price: 11.9,
+        img: 'plate__chicken-salad'
+    },
+    [crypto.randomUUID()]: {
+        name: 'Ravioli al Burro',
+        price: 9.9,
+        img: 'plate__ravioli'
+    },
+    [crypto.randomUUID()]: {
+        name: 'Fake Fish and Chips',
+        price: 4.9,
+        img: 'plate__fish-sticks-fries'
+    },
+    [crypto.randomUUID()]: {
+        name: 'Tortellini Alfredo',
+        price: 9.9,
+        img: 'plate__tortellini'
+    }
+};
+
+export default inventory;
